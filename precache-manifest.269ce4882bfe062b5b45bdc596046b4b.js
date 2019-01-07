077addf6dc7819b49463fbecf477ce4284f5e090
shayannasir <sshayann7@gmail.com>
2019-01-08 05:13:40 +0530

    Updates

---
+++ precache-manifest.269ce4882bfe062b5b45bdc596046b4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "a64255914363f1a05265",
+    "url": "/contactmanager/static/js/main.a6425591.chunk.js"
+  },
+  {
+    "revision": "1a209844adedbd57959d",
+    "url": "/contactmanager/static/js/1.1a209844.chunk.js"
+  },
+  {
+    "revision": "a64255914363f1a05265",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "1a209844adedbd57959d",
+    "url": "/contactmanager/static/css/1.2e28284a.chunk.css"
+  },
+  {
+    "revision": "f88c0a0b92e40783f75395a0d5fad8a7",
+    "url": "/contactmanager/index.html"
+  }
+];
