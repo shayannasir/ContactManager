7722c30db941cb9f8ac7950d51aff21859e29850
Syed Shayan Nasir <35117440+shayannasir@users.noreply.github.com>
2019-01-08 05:17:31 +0530

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <h1 className="display-4">About Contacts Manager</h1>
+      <h1 className="display-4">About Contact Manager</h1>
       <p className="lead">Simple App to manage Contacts</p>
       <p className="text-secondary">Version 1.0.0</p>
     </div>
